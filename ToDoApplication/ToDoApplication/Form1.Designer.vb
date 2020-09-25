82cb9a73856ffc41bec13603f24a25853d6606a6
itotono <35085212+itotono@users.noreply.github.com>
2020-09-25 14:35:15 +0900

    Add files via upload

---
+++ ToDoApplication/ToDoApplication/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.lstToDo = New System.Windows.Forms.ListBox()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'lstToDo
+        '
+        Me.lstToDo.FormattingEnabled = True
+        Me.lstToDo.ItemHeight = 12
+        Me.lstToDo.Location = New System.Drawing.Point(12, 12)
+        Me.lstToDo.Name = "lstToDo"
+        Me.lstToDo.Size = New System.Drawing.Size(340, 196)
+        Me.lstToDo.TabIndex = 0
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(439, 108)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(75, 23)
+        Me.btnDelete.TabIndex = 1
+        Me.btnDelete.Text = "削除"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(439, 37)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.TabIndex = 2
+        Me.btnAdd.Text = "追加"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(358, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(156, 19)
+        Me.TextBox1.TabIndex = 3
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(439, 185)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "終了"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(439, 83)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(75, 19)
+        Me.TextBox2.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(519, 219)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.lstToDo)
+        Me.Name = "Form1"
+        Me.Text = "今日のタス君"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents btnExit As Button
+    Public WithEvents lstToDo As ListBox
+    Friend WithEvents TextBox2 As TextBox
+End Class
---
+++ ToDoApplication/ToDoApplication/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        lstToDo.Items.Add(TextBox1.Text)
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        Dim bangou As Byte
+        bangou = TextBox2.Text - 1
+
+        lstToDo.Items.RemoveAt(bangou)
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit()
+    End Sub
+End Class
